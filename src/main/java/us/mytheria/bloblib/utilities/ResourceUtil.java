462ed28daaec56450c5f6857a306b48619648be9
lbenav8095 <lubensaico@gmail.com>
2022-07-28 16:37:05 -0600

    1.2

--- src/main/java/us/mytheria/bloblib/utilities/ResourceUtil.java
+++ src/main/java/us/mytheria/bloblib/utilities/ResourceUtil.java
@@ -1,5 +1,8 @@
 package us.mytheria.bloblib.utilities;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,9 +15,9 @@ public class ResourceUtil {
                 File parentFile = file.getParentFile();
                 parentFile.mkdirs();
             }
-            if (!file.exists()) {
+            if (!file.exists())
                 file.createNewFile();
-            } else
+            else
                 return;
             FileOutputStream fos = new FileOutputStream(file);
             byte[] ba = inputStream.readAllBytes();
@@ -23,5 +26,40 @@ public class ResourceUtil {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * @param existingFile         The file to be overwritten
+     * @param newYamlConfiguration The new configuration to be written to the file
+     */
+    public static void writeNewValues(File existingFile, YamlConfiguration newYamlConfiguration) {
+        FileConfiguration existingFileConfiguration = YamlConfiguration.loadConfiguration(existingFile);
+        for (String section : newYamlConfiguration.getConfigurationSection("").getKeys(true)) {
+            if (existingFileConfiguration.get(section) != null) continue;
+
+            existingFileConfiguration.set(section, newYamlConfiguration.get(section));
+        }
+
+        try {
+            existingFileConfiguration.save(existingFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void inputStreamToFile(File file, InputStream inputStream) {
+        try {
+            if (!file.exists())
+                file.createNewFile();
+            else
+                return;
+            FileOutputStream fos = new FileOutputStream(file);
+            byte[] ba = inputStream.readAllBytes();
+            fos.write(ba);
+            fos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
