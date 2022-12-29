8894cae24252c7d88f081ca3b7bec9c561379749
lbenav8095 <lubensaico@gmail.com>
2022-12-29 03:38:55 -0500

    Added shortcut for listeners

--- src/main/java/us/mytheria/bloblib/BlobLibDevAPI.java
+++ src/main/java/us/mytheria/bloblib/BlobLibDevAPI.java
@@ -1,11 +1,18 @@
 package us.mytheria.bloblib;
 
 import me.anjoismysign.anjo.entities.Result;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import us.mytheria.bloblib.entities.listeners.BlobChatListener;
+import us.mytheria.bloblib.entities.listeners.BlobDropListener;
+import us.mytheria.bloblib.entities.listeners.BlobSelPosListener;
 import us.mytheria.bloblib.utilities.ResourceUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.function.Consumer;
 
 public class BlobLibDevAPI {
     private static final BlobLib main = BlobLib.getInstance();
@@ -78,5 +85,48 @@ public class BlobLibDevAPI {
             }
         }
         return Result.invalid(file);
+    }
+
+    /**
+     * Adds a new chat listener
+     *
+     * @param player            The player
+     * @param timeout           The timeout
+     * @param consumer          The consumer. The String is the message sent by the player
+     * @param timeoutMessageKey The timeout message key
+     * @param timerMessageKey   The timer message key
+     */
+    public static void addChatListener(Player player, long timeout, Consumer<String> consumer,
+                                       String timeoutMessageKey, String timerMessageKey) {
+        BlobLib.getInstance().getChatManager().addChatListener(player,
+                BlobChatListener.smart(player, timeout, consumer, timeoutMessageKey, timerMessageKey));
+    }
+
+    /**
+     * Adds a new drop listener
+     *
+     * @param player          The player
+     * @param consumer        The consumer. The ItemStack is the item dropped.
+     * @param timerMessageKey The timer message key
+     */
+    public static void addDropListener(Player player, Consumer<ItemStack> consumer,
+                                       String timerMessageKey) {
+        BlobLib.getInstance().getDropListenerManager().addDropListener(player,
+                BlobDropListener.smart(player, consumer, timerMessageKey));
+    }
+
+    /**
+     * Adds a new position listener
+     *
+     * @param player            The player
+     * @param timeout           The timeout
+     * @param consumer          The consumer. The Block is the block clicked.
+     * @param timeoutMessageKey The timeout message key
+     * @param timerMessageKey   The timer message key
+     */
+    public static void addPositionListener(Player player, long timeout, Consumer<Block> consumer,
+                                           String timeoutMessageKey, String timerMessageKey) {
+        BlobLib.getInstance().getPositionManager().addPositionListener(player,
+                BlobSelPosListener.smart(player, timeout, consumer, timeoutMessageKey, timerMessageKey));
     }
 }
