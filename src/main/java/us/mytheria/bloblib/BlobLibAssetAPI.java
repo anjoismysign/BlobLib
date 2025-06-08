f5d8906b2f9adb7762a105ec70ad069fea0bf18c
Luis Benavides Naranjo <lubensaico@gmail.com>
2025-06-08 16:19:19 -0600

    Refactor BlobLibAssetAPI to remove deprecated methods and streamline constructor

--- src/main/java/us/mytheria/bloblib/BlobLibAssetAPI.java
+++ src/main/java/us/mytheria/bloblib/BlobLibAssetAPI.java
@@ -1,29 +1,12 @@
 package us.mytheria.bloblib;
 
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.jetbrains.annotations.Nullable;
-import us.mytheria.bloblib.action.Action;
 import us.mytheria.bloblib.api.BlobLibActionAPI;
 import us.mytheria.bloblib.api.BlobLibInventoryAPI;
 import us.mytheria.bloblib.api.BlobLibMessageAPI;
 import us.mytheria.bloblib.api.BlobLibSoundAPI;
 import us.mytheria.bloblib.api.BlobLibTagAPI;
 import us.mytheria.bloblib.api.BlobLibTranslatableAPI;
-import us.mytheria.bloblib.entities.inventory.BlobInventory;
-import us.mytheria.bloblib.entities.inventory.InventoryBuilderCarrier;
-import us.mytheria.bloblib.entities.inventory.InventoryButton;
-import us.mytheria.bloblib.entities.inventory.MetaBlobInventory;
-import us.mytheria.bloblib.entities.inventory.MetaInventoryButton;
-import us.mytheria.bloblib.entities.message.BlobSound;
 import us.mytheria.bloblib.itemapi.ItemMaterialManager;
-import us.mytheria.bloblib.managers.ActionManager;
-import us.mytheria.bloblib.managers.InventoryManager;
-import us.mytheria.bloblib.managers.MessageManager;
-import us.mytheria.bloblib.managers.MetaInventoryShard;
-import us.mytheria.bloblib.managers.SoundManager;
-
-import java.util.Optional;
 
 /**
  * Please use the API classes instead of this class.
@@ -38,17 +21,6 @@ public class BlobLibAssetAPI {
     private final BlobLibTranslatableAPI translatableAPI;
     private final ItemMaterialManager materialManager;
 
-    private BlobLibAssetAPI(BlobLib plugin) {
-        this.soundAPI = BlobLibSoundAPI.getInstance(plugin);
-        this.inventoryAPI = BlobLibInventoryAPI.getInstance(plugin);
-        this.actionAPI = BlobLibActionAPI.getInstance(plugin);
-        this.tagAPI = BlobLibTagAPI.getInstance(plugin);
-        this.messageAPI = BlobLibMessageAPI.getInstance(plugin);
-        this.translatableAPI = BlobLibTranslatableAPI.getInstance(plugin);
-        this.materialManager = new ItemMaterialManager() {
-        };
-    }
-
     public static BlobLibAssetAPI getInstance(BlobLib plugin) {
         if (instance == null) {
             if (plugin == null)
@@ -60,6 +32,17 @@ public class BlobLibAssetAPI {
 
     public static BlobLibAssetAPI getInstance() {
         return getInstance(null);
+    }
+
+    private BlobLibAssetAPI(BlobLib plugin) {
+        this.soundAPI = BlobLibSoundAPI.getInstance(plugin);
+        this.inventoryAPI = BlobLibInventoryAPI.getInstance(plugin);
+        this.actionAPI = BlobLibActionAPI.getInstance(plugin);
+        this.tagAPI = BlobLibTagAPI.getInstance(plugin);
+        this.messageAPI = BlobLibMessageAPI.getInstance(plugin);
+        this.translatableAPI = BlobLibTranslatableAPI.getInstance(plugin);
+        this.materialManager = new ItemMaterialManager() {
+        };
     }
 
     public BlobLibSoundAPI getSoundAPI() {
@@ -90,78 +73,4 @@ public class BlobLibAssetAPI {
         return materialManager;
     }
 
-    @Deprecated
-    public static InventoryManager getInventoryManager() {
-        return BlobLibInventoryAPI.getInstance().getInventoryManager();
-    }
-
-    @Deprecated
-    public static MessageManager getMessageManager() {
-        return BlobLibMessageAPI.getInstance().getMessageManager();
-    }
-
-    @Deprecated
-    public static ActionManager getActionManager() {
-        return BlobLibActionAPI.getInstance().getActionManager();
-    }
-
-    @Deprecated
-    public static SoundManager getSoundManager() {
-        return BlobLibSoundAPI.getInstance().getSoundManager();
-    }
-
-    @Deprecated
-    public static InventoryBuilderCarrier<InventoryButton> getInventoryBuilderCarrier(String key) {
-        return getInventoryManager().getInventoryBuilderCarrier(key);
-    }
-
-    @Deprecated
-    public static BlobInventory getBlobInventory(String key) {
-        return getInventoryManager().getInventory(key);
-    }
-
-    @Deprecated
-    public static InventoryBuilderCarrier<MetaInventoryButton> getMetaInventoryBuilderCarrier(String key) {
-        return getInventoryManager().getMetaInventoryBuilderCarrier(key);
-    }
-
-    @Deprecated
-    public static MetaBlobInventory getMetaBlobInventory(String key) {
-        return getInventoryManager().getMetaInventory(key);
-    }
-
-    @Deprecated
-    public static Optional<MetaInventoryShard> hasMetaInventoryShard(String type) {
-        return Optional.ofNullable(getInventoryManager().getMetaInventoryShard(type));
-    }
-
-    @Deprecated
-    @Nullable
-    public static Action<Entity> getAction(String key) {
-        return getActionManager().getAction(key);
-    }
-
-    @Deprecated
-    public static void sendMessage(String key, Player player) {
-        getMessageManager().send(player, key);
-    }
-
-    @Deprecated
-    public static BlobSound getSound(String key) {
-        return getSoundManager().getSound(key);
-    }
-
-    @Deprecated
-    public static void playSound(String key, Player player) {
-        getSoundManager().play(player, key);
-    }
-
-    @Deprecated
-    public static BlobInventory buildInventory(String fileName) {
-        BlobInventory inventory = BlobLibAssetAPI.getInventoryManager().cloneInventory(fileName);
-        if (inventory == null) {
-            throw new NullPointerException("Inventory '" + fileName + "' not found");
-        }
-        return inventory;
-    }
 }
