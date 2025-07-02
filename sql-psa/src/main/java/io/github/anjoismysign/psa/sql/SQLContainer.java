c57f345e3bc41285074f59a1e9ad3652489b9bf2
Luis Benavides Naranjo <lubensaico@gmail.com>
2025-07-02 09:32:05 -0600

    refactor: update dependencies in pom.xml to remove unused entries and correct artifact name

--- sql-psa/src/main/java/io/github/anjoismysign/psa/sql/SQLContainer.java
+++
@@ -1,7 +0,0 @@
-package io.github.anjoismysign.psa.sql;
-
-public interface SQLContainer {
-    SQLDatabase getDatabase();
-
-    void disconnect();
-}
--- sql-psa/src/main/java/io/github/anjoismysign/psa/sql/SQLCrudDatabase.java
+++
@@ -1,9 +0,0 @@
-package io.github.anjoismysign.psa.sql;
-
-import io.github.anjoismysign.psa.crud.CrudDatabase;
-import org.jetbrains.annotations.NotNull;
-
-public interface SQLCrudDatabase extends CrudDatabase {
-    @NotNull
-    SQLContainer generateContainer();
-}
