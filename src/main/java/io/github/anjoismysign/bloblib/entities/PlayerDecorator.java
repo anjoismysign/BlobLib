8a19323dcb0613339bf1f9d8fba5b8b20d6231b5
anjoismysign <lubensaico@gmail.com>
2026-01-27 12:58:51 -0600

    Add validity supplier to PlayerDecorator and update constructors.

--- src/main/java/io/github/anjoismysign/bloblib/entities/PlayerDecorator.java
+++ src/main/java/io/github/anjoismysign/bloblib/entities/PlayerDecorator.java
@@ -4,12 +4,18 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+import java.util.function.Supplier;
 
-public record PlayerDecorator(@NotNull PlayerAddress address) implements AddressDecorator<Player> {
+public record PlayerDecorator(@NotNull PlayerAddress address,
+                              @NotNull Supplier<Boolean> validSupplier) implements AddressDecorator<Player> {
+
+    public static PlayerDecorator of(@NotNull Player player){
+        return new PlayerDecorator(PlayerAddress.of(player.getUniqueId().toString()), player::isConnected);
+    }
 
     public static PlayerDecorator address(@NotNull PlayerAddress address) {
         Objects.requireNonNull(address, "'address' is null");
-        return new PlayerDecorator(address);
+        return new PlayerDecorator(address, () -> address.look() != null);
     }
 
     @NotNull
@@ -23,6 +29,11 @@ public record PlayerDecorator(@NotNull PlayerAddress address) implements Address
         if (lookup == null)
             throw new NullPointerException("'lookup' is null");
         return PermissibleDecorator.of(AddressFactory.getInstance().of(lookup));
+    }
+
+    @Override
+    public boolean isValid(){
+        return validSupplier.get();
     }
 
 }
