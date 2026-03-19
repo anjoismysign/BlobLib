acdb42f1898030f9afbc5aa919990603e033682b
anjoismysign <lubensaico@gmail.com>
2026-03-19 15:09:51 -0600

    Removed useless import

--- src/main/java/io/github/anjoismysign/bloblib/entities/listeners/BlobSelectorListener.java
+++ src/main/java/io/github/anjoismysign/bloblib/entities/listeners/BlobSelectorListener.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.function.Consumer;
 
 public class BlobSelectorListener<T> extends SelectorListener<T> {
