bdd454bef19a4285b4712c17c85405137bfd6da4
anjoismysign <lubensaico@gmail.com>
2026-05-26 12:59:48 -0600

    Improves AttributeModifierBean

--- src/main/java/io/github/anjoismysign/bloblib/entities/AttributeModifierBean.java
+++ src/main/java/io/github/anjoismysign/bloblib/entities/AttributeModifierBean.java
@@ -1,18 +1,30 @@
 package io.github.anjoismysign.bloblib.entities;
 
+import net.kyori.adventure.key.Key;
+import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.EquipmentSlotGroup;
+
+import java.util.Locale;
 
 public class AttributeModifierBean {
-        private double amount;
+        private String key;
         private AttributeModifier.Operation operation;
-        private String attribute;
+        private double amount;
+        private String equipmentSlotGroup;
 
-        public double getAmount() {
-                return amount;
+        public AttributeModifier toAttributeModifier(){
+                Key key = Key.key(this.key);
+                NamespacedKey namespacedKey = new NamespacedKey(key.namespace(), key.value());
+                return new AttributeModifier(namespacedKey, amount, operation, EquipmentSlotGroup.getByName(equipmentSlotGroup.toLowerCase(Locale.ROOT)));
         }
 
-        public void setAmount(double amount) {
-                this.amount = amount;
+        public String getKey() {
+                return key;
+        }
+
+        public void setKey(String key) {
+                this.key = key;
         }
 
         public AttributeModifier.Operation getOperation() {
@@ -23,11 +35,19 @@ public class AttributeModifierBean {
                 this.operation = operation;
         }
 
-        public String getAttribute() {
-                return attribute;
+        public double getAmount() {
+                return amount;
         }
 
-        public void setAttribute(String attribute) {
-                this.attribute = attribute;
+        public void setAmount(double amount) {
+                this.amount = amount;
+        }
+
+        public String getEquipmentSlotGroup() {
+                return equipmentSlotGroup;
+        }
+
+        public void setEquipmentSlotGroup(String equipmentSlotGroup) {
+                this.equipmentSlotGroup = equipmentSlotGroup;
         }
 }
