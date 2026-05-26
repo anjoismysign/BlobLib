a77220a135598237d5bcf58c328124a220231553
anjoismysign <lubensaico@gmail.com>
2026-05-26 13:30:04 -0600

    Adds static #deserializeAttributes method to AttributeModifierBean

--- src/main/java/io/github/anjoismysign/bloblib/entities/AttributeModifierBean.java
+++ src/main/java/io/github/anjoismysign/bloblib/entities/AttributeModifierBean.java
@@ -1,11 +1,18 @@
 package io.github.anjoismysign.bloblib.entities;
 
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
 import net.kyori.adventure.key.Key;
 import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.EquipmentSlotGroup;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class AttributeModifierBean {
         private String key;
@@ -20,6 +27,19 @@ public class AttributeModifierBean {
                 bean.setAmount(attributeModifier.getAmount());
                 bean.setEquipmentSlotGroup(attributeModifier.getSlotGroup().toString());
                 return bean;
+        }
+
+        public static Map<Attribute, AttributeModifier> deserializeAttributes(@NotNull Map<String, AttributeModifierBean> serialized){
+                Map<Attribute, AttributeModifier> attributes = new HashMap<>();
+                var registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.ATTRIBUTE);
+                serialized.forEach((attributeKey, bean)->{
+                        @Nullable Attribute attribute = registry.get(Key.key(attributeKey));
+                        if (attribute == null){
+                                throw new NullPointerException("Cannot find Attribute by Key '"+attributeKey+"'");
+                        }
+                        attributes.put(attribute, bean.toAttributeModifier());
+                });
+                return attributes;
         }
 
         public AttributeModifier toAttributeModifier(){
