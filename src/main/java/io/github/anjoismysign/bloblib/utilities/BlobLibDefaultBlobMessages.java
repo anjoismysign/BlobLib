861330939b6fbebfae3135fe1fe2d32a5d9ea58e
anjoismysign <lubensaico@gmail.com>
2026-06-11 18:13:43 -0600

    Adds locale localization to economy not-enough messages.

--- src/main/java/io/github/anjoismysign/bloblib/utilities/BlobLibDefaultBlobMessages.java
+++ src/main/java/io/github/anjoismysign/bloblib/utilities/BlobLibDefaultBlobMessages.java
@@ -2,6 +2,7 @@ package io.github.anjoismysign.bloblib.utilities;
 
 import io.github.anjoismysign.bloblib.api.BlobLibEconomyAPI;
 import io.github.anjoismysign.bloblib.entities.message.BlobMessage;
+import io.github.anjoismysign.bloblib.managers.BlobLibConfigManager;
 import io.github.anjoismysign.bloblib.vault.multieconomy.ElasticEconomy;
 import net.milkbowl.vault.economy.IdentityEconomy;
 import org.bukkit.command.CommandSender;
@@ -14,9 +15,11 @@ public enum BlobLibDefaultBlobMessages implements DefaultBlobMessages{
 
     @Override
     public void economyNotEnough(@NotNull Player player, double missingAmount, @Nullable String currency) {
+        String locale = player.getLocale();
         ElasticEconomy elasticEconomy = BlobLibEconomyAPI.getInstance().getElasticEconomy();
         IdentityEconomy economy = currency == null ? elasticEconomy.getDefault() : elasticEconomy.getImplementation(currency);
         BlobMessage.by("Economy.Not-Enough")
+                .localize(locale)
                 .modder()
                 .replace("%display%", economy.format(missingAmount))
                 .get()
@@ -25,9 +28,11 @@ public enum BlobLibDefaultBlobMessages implements DefaultBlobMessages{
 
     @Override
     public void economyNotEnoughOthers(@NotNull Player player, double missingAmount, @Nullable String currency, @NotNull CommandSender receiver) {
+        String locale = (receiver instanceof Player playerCommandSender) ? playerCommandSender.getLocale() : BlobLibConfigManager.getInstance().getConsoleLocale();
         ElasticEconomy elasticEconomy = BlobLibEconomyAPI.getInstance().getElasticEconomy();
         IdentityEconomy economy = currency == null ? elasticEconomy.getDefault() : elasticEconomy.getImplementation(currency);
         BlobMessage.by("Economy.Not-Enough-Others")
+                .localize(locale)
                 .modder()
                 .replace("%player%", player.getName())
                 .replace("%display%", economy.format(missingAmount))
